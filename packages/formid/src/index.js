f7e79e854a3817153456debe5a1724721c49755e
方永乐 <fangyongle@innobuddy.com>
2018-11-13 14:09:12 +0800

    feat: formID

--- packages/formid/src/index.js
+++ packages/formid/src/index.js
@@ -1,90 +1,107 @@
-const app = getApp()
+const app = getApp();
 Component({
   properties: {
-    data: {
-      type: null
+    payload: {
+      type: null,
+      value: {}
     },
-    args: {
-      type: null
+    openType: {
+      type: String,
+      value: ''
+    },
+    className: {
+      type: String,
+      value: ''
     },
     longPressDuration: {
       type: Number,
-      default: 0
+      value: 0
     }
   },
   data: {
-    touchStartTime: 0,
+    touchStartTime: 0
   },
   timer: null,
   methods: {
     formSubmit(e) {
-      const formId = e.detail.formId
-      console.log('拿到 formId: ', formId)
-      app.formIds.add(formId)
+      const formId = e.detail.formId;
+
+      // collect form id
+      if (app.onGetFormId instanceof Function) {
+        app.onGetFormId(formId);
+      }
       if (this.data.longPressDuration === 0) {
-        this.triggerClick()
+        this.triggerClick(e);
       }
     },
-    touchstart() {
-      console.log('%c开始按住，%dms 后触发事件', 'color: red', this.data.longPressDuration)
-      this.setData({touchStartTime: Date.now()})
+    touchstart(e) {
+      this.setData({ touchStartTime: Date.now() });
       if (this.timer) {
-        clearTimeout(this.timer)
+        clearTimeout(this.timer);
       }
-      this.triggerEvent('touch', {
-        data: this.data.data,
-        args: this.data.args,
-      }, {
-        bubbles: true,
-        composed: false
-      })
+      this.triggerEvent(
+        'touch',
+        {
+          event: e,
+          payload: this.data.payload
+        },
+        {
+          bubbles: true,
+          composed: false
+        }
+      );
       if (this.data.longPressDuration !== 0) {
         this.timer = setTimeout(() => {
-          this.timer = null
-          this.triggerClick()
-        }, this.data.longPressDuration || 50)
+          this.timer = null;
+          this.triggerClick(e);
+        }, this.data.longPressDuration || 50);
       }
     },
     touchcancel() {
       if (this.timer) {
-        clearTimeout(this.timer)
+        clearTimeout(this.timer);
       }
 
-      this.triggerEvent('cancel', {
-        data: this.data.data,
-        args: this.data.args,
-      }, {
-        bubbles: true,
-        composed: false
-      })
-      this.setData({touchStartTime: 0})
+      this.triggerEvent(
+        'cancel',
+        {},
+        {
+          bubbles: true,
+          composed: false
+        }
+      );
+      this.setData({ touchStartTime: 0 });
     },
     touchend() {
       if (this.timer) {
-        clearTimeout(this.timer)
+        clearTimeout(this.timer);
       }
 
-      this.triggerEvent('end', {
-        data: this.data.data,
-        args: this.data.args,
-      }, {
-        bubbles: true,
-        composed: false
-      })
-      this.setData({touchStartTime: 0})
+      this.triggerEvent(
+        'end',
+        {},
+        {
+          bubbles: true,
+          composed: false
+        }
+      );
+      this.setData({ touchStartTime: 0 });
     },
-    // 触发点击事件
-    triggerClick() {
+    triggerClick(e) {
       if (this.timer) {
-        clearTimeout(this.timer)
+        clearTimeout(this.timer);
       }
-      this.triggerEvent('click', {
-        data: this.data.data,
-        args: this.data.args,
-      }, {
-        bubbles: true,
-        composed: false
-      })
+      this.triggerEvent(
+        'click',
+        {
+          event: e,
+          payload: this.data.payload
+        },
+        {
+          bubbles: true,
+          composed: false
+        }
+      );
     }
   }
-})
+});
