a66e29b4a65d7d40d0cc237d7214735810e6ce8f
方永乐 <fangyongle@innobuddy.com>
2018-11-13 14:12:58 +0800

    feat: formid

--- packages/scroll-view/tools/config.js
+++ packages/scroll-view/tools/config.js
@@ -1,15 +1,15 @@
-const path = require('path')
+const path = require('path');
 
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
 
-const isDev = process.argv.indexOf('--develop') >= 0
-const isWatch = process.argv.indexOf('--watch') >= 0
-const demoSrc = path.resolve(__dirname, './demo')
-const demoDist = path.resolve(__dirname, '../miniprogram_dev')
-const src = path.resolve(__dirname, '../src')
-const dev = path.join(demoDist, 'components')
-const dist = path.resolve(__dirname, '../miniprogram_dist')
+const isDev = process.argv.indexOf('--develop') >= 0;
+const isWatch = process.argv.indexOf('--watch') >= 0;
+const demoSrc = path.resolve(__dirname, './demo');
+const demoDist = path.resolve(__dirname, '../miniprogram_dev');
+const src = path.resolve(__dirname, '../src');
+const dev = path.join(demoDist, 'components');
+const dist = path.resolve(__dirname, '../miniprogram_dist');
 
 module.exports = {
   entry: ['index'],
@@ -39,7 +39,10 @@ module.exports = {
       rules: [
         {
           test: /\.js$/i,
-          use: ['babel-loader', 'eslint-loader'],
+          use: [
+            'babel-loader'
+            // 'eslint-loader'
+          ],
           exclude: /node_modules/
         }
       ]
@@ -50,7 +53,7 @@ module.exports = {
     },
     plugins: [
       new webpack.DefinePlugin({}),
-      new webpack.optimize.LimitChunkCountPlugin({maxChunks: 1})
+      new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 })
     ],
     optimization: {
       minimize: false
@@ -58,9 +61,9 @@ module.exports = {
     // devtool: 'nosources-source-map', // source map for js
     performance: {
       hints: 'warning',
-      assetFilter: (assetFilename) => assetFilename.endsWith('.js')
+      assetFilter: assetFilename => assetFilename.endsWith('.js')
     }
   },
 
   copy: ['./wxml', './wxss', './wxs', './images'] // will copy to dist
-}
+};
