1f5153c62f21295917f7084501fbda8dbe8faf82
方永乐 <fangyongle@innobuddy.com>
2018-11-13 11:45:14 +0800

    feat: formid

--- packages/formid/tools/config.js
+++ packages/formid/tools/config.js
@@ -1,15 +1,15 @@
-const path = require('path')
+const path = require('path');
 
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
 
-const isDev = process.argv.indexOf('--develop') >= 0
-const isWatch = process.argv.indexOf('--watch') >= 0
-const demoSrc = path.resolve(__dirname, './demo')
-const demoDist = path.resolve(__dirname, '../miniprogram_dev')
-const src = path.resolve(__dirname, '../src')
-const dev = path.join(demoDist, 'components')
-const dist = path.resolve(__dirname, '../miniprogram_dist')
+const isDev = process.argv.indexOf('--develop') >= 0;
+const isWatch = process.argv.indexOf('--watch') >= 0;
+const demoSrc = path.resolve(__dirname, './demo');
+const demoDist = path.resolve(__dirname, '../miniprogram_dev');
+const src = path.resolve(__dirname, '../src');
+const dev = path.join(demoDist, 'components');
+const dist = path.resolve(__dirname, '../miniprogram_dist');
 
 module.exports = {
   entry: ['index'],
@@ -24,37 +24,39 @@ module.exports = {
 
   wxss: {
     less: false, // compile wxss with less
-    sourcemap: false, // source map for less
+    sourcemap: false // source map for less
   },
 
   webpack: {
     mode: 'production',
     output: {
       filename: '[name].js',
-      libraryTarget: 'commonjs2',
+      libraryTarget: 'commonjs2'
     },
     target: 'node',
     externals: [nodeExternals()], // ignore node_modules
     module: {
-      rules: [{
-        test: /\.js$/i,
-        use: [
-          'babel-loader',
-          'eslint-loader'
-        ],
-        exclude: /node_modules/
-      }],
+      rules: [
+        {
+          test: /\.js$/i,
+          use: [
+            'babel-loader'
+            // 'eslint-loader'
+          ],
+          exclude: /node_modules/
+        }
+      ]
     },
     resolve: {
       modules: [src, 'node_modules'],
-      extensions: ['.js', '.json'],
+      extensions: ['.js', '.json']
     },
     plugins: [
       new webpack.DefinePlugin({}),
-      new webpack.optimize.LimitChunkCountPlugin({maxChunks: 1}),
+      new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 })
     ],
     optimization: {
-      minimize: false,
+      minimize: false
     },
     // devtool: 'nosources-source-map', // source map for js
     performance: {
@@ -63,5 +65,5 @@ module.exports = {
     }
   },
 
-  copy: ['./wxml', './wxss', './wxs', './images'], // will copy to dist
-}
+  copy: ['./wxml', './wxss', './wxs', './images'] // will copy to dist
+};
